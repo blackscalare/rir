16f5843279ec473474f2dca1d616ddac9a89f769
blackscalare <blackscalare@gmail.com>
2025-05-12 00:34:04 +0200

    moved everything to renderer

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
-use raylib::prelude::*;
 mod render;
-pub mod utils;
+mod utils;
 
 const WIDTH: i32 = 640;
 const HEIGHT: i32 = 480;
 
 fn main() {
-    let (mut rl, thread) = raylib::init()
-        .size(WIDTH, HEIGHT)
-        .title("Hello World")
-        .build();
-
-    let text = "Hello, world!";
-
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
-
-        d.clear_background(Color::WHITE);
-        let text_center = utils::center(WIDTH, HEIGHT, text.len() as i32, 20);
-
-        d.draw_text(text, text_center.0, text_center.1, 20, Color::BLACK);
-        d.draw_fps(WIDTH - 50, 10);
-    }
+    let mut renderer = render::Renderer::new(WIDTH, HEIGHT);
+    renderer.init();
+    renderer.start();
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,3 +1,4 @@
+use crate::utils;
 use raylib::prelude::*;
 
 pub struct Renderer {
@@ -27,13 +28,20 @@ impl Renderer {
         self.rl_thread = Some(thread);
     }
 
-    pub fn start(&self) {
-        if let (Some(handle)) = &self.rl_handle {
-            while !handle.window_should_close() {
-                &self.draw();
-            }
+    pub fn start(&mut self) {
+        let (handle, thread) = (
+            self.rl_handle.as_mut().expect("Handle not initialized"),
+            self.rl_thread.as_mut().expect("Thread not initialized"),
+        );
+
+        let text = "Test";
+
+        while !handle.window_should_close() {
+            let mut draw_handle = handle.begin_drawing(thread);
+            draw_handle.clear_background(Color::WHITE);
+            let text_center_x_y = utils::center(self.width, self.height, text.len() as i32, 20);
+            draw_handle.draw_text(text, text_center_x_y.0, text_center_x_y.1, 20, Color::BLACK);
+            draw_handle.draw_fps(self.width - 50, 10);
         }
     }
-
-    fn draw(&self) {}
 }
