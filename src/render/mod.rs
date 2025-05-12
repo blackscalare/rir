f92e87704b73644733780a689ea782b6a8e0e770
blackscalare <blackscalare@gmail.com>
2025-05-13 01:10:25 +0200

    better rendering handling

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,3 +1,4 @@
+use crate::constants;
 use crate::utils;
 use raylib::prelude::*;
 
@@ -28,20 +29,33 @@ impl Renderer {
         self.rl_thread = Some(thread);
     }
 
+    // Checks if handle and thread is initialized, and starts the drawing.
+    // Blocks the thread so use in the last spot
+    // TODO do not block main thread
+    //      or create threads where everything else like input is handled
     pub fn start(&mut self) {
         let (handle, thread) = (
             self.rl_handle.as_mut().expect("Handle not initialized"),
             self.rl_thread.as_mut().expect("Thread not initialized"),
         );
 
-        let text = "Test";
+        Self::draw(handle, thread, self.width, self.height);
+    }
 
+    fn draw(handle: &mut RaylibHandle, thread: &RaylibThread, width: i32, height: i32) {
         while !handle.window_should_close() {
             let mut draw_handle = handle.begin_drawing(thread);
             draw_handle.clear_background(Color::WHITE);
-            let text_center_x_y = utils::center(self.width, self.height, text.len() as i32, 20);
-            draw_handle.draw_text(text, text_center_x_y.0, text_center_x_y.1, 20, Color::BLACK);
-            draw_handle.draw_fps(self.width - 50, 10);
+            let text_center_x_y =
+                utils::center(width, height, constants::texts::TEST_TEXT.len() as i32, 20);
+            draw_handle.draw_text(
+                constants::texts::TEST_TEXT,
+                text_center_x_y.0,
+                text_center_x_y.1,
+                20,
+                Color::BLACK,
+            );
+            draw_handle.draw_fps(width - 50, 10);
         }
     }
 }
