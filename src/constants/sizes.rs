a5b0fac719b6d72157623f93827eb4b1915afd23
blackscalare <blackscalare@gmail.com>
2025-06-02 00:05:50 +0200

    more constants

--- src/constants/sizes.rs
+++ src/constants/sizes.rs
@@ -6,5 +6,9 @@ pub const WINDOW_HEIGHT: i32 = 720;
 pub const PLAYER_WIDTH: i32 = 128;
 pub const PLAYER_HEIGHT: i32 = 128;
 
+// Tree consts
+pub const TREE_WIDTH: i32 = 128;
+pub const TREE_HEIGHT: i32 = 128;
+
 // Inventory consts
 pub const INVENTORY_SIZE_START: u32 = 10;
--- src/constants/stats.rs
+++ src/constants/stats.rs
@@ -1,3 +1,6 @@
 // Health
 pub const BLOB_START_HEALTH: u32 = 100;
 pub const SMALL_TREE_HEALTH: u32 = 100;
+
+// Damage
+pub const DEFAULT_AXE_DAMAGE: u32 = 10;
